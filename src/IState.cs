7790da31c1fee591f82764a10ab66ccfbe626e62
focusedgamedev <team@thefocusedgamedev.com>
2020-12-27 10:40:48 -0600

    Initial content commit.

---
+++ src/IState.cs
@@ -0,0 +1,9 @@
+﻿namespace SimpleStateMachine
+{
+	public interface IState
+	{
+		void OnStart( StateMachine sm );
+		void Update();
+		void OnFinish();
+	}
+}
---
+++ src/StateMachine.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SimpleStateMachine
+{
+	public class StateMachine
+	{
+		Dictionary<int, IState> m_states = new Dictionary<int, IState>();
+
+		IState m_currentState = null;
+		IState m_nextState = null;
+
+		public void AddState( int key, IState state )
+		{
+			try
+			{
+				m_states.Add( key, state );
+			}
+			catch( Exception e )
+			{
+				throw e;
+			}
+		}
+
+		public void SetState( int key )
+		{
+			IState state;
+			if ( m_states.TryGetValue( key, out state ) )
+			{
+				if ( m_currentState != null )
+				{
+					m_currentState.OnFinish();
+				}
+
+				m_nextState = state;
+			}
+		}
+
+		public void Update()
+		{
+			if ( m_nextState != null )
+			{
+				m_nextState.OnStart( this );
+				m_currentState = m_nextState;
+				m_nextState = null;
+			}
+
+			if ( m_currentState != null )
+			{
+				m_currentState.Update();
+			}
+		}
+
+		public void Shutdown()
+		{
+			if (m_currentState != null)
+			{
+				m_currentState.OnFinish();
+				m_currentState = null;
+			}
+
+			m_states.Clear();
+		}
+	}
+}
